8ed9d4128f132cd23b152c5f0cb4445368860d08
majesuso <majesus.op@gmail.com>
2022-07-21 12:38:43 -0400

    obteniendo número al azar

--- src/app.js
+++ src/app.js
@@ -1,9 +1,39 @@
-//aquí va la función para crear el juego
-//probando
-//probando x2
-
+// valores botones usuario
 const rockValue = document.getElementById("rock");
+const paperValue = document.getElementById("paper");
+const sicsorsValue = document.getElementById("sicsors");
+// valor opción seleccionada
+let choiceBtn = document.getElementById("choice");
+
+// Función elección PC
+function getRandomChoice(){
+    // Obtener un número random entre 1 y 3
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    return randomNumber
+}
+
+
+// escuchar la opción marcada por el usuario
 rockValue.addEventListener("click", function(e) {
     e.preventDefault();
     console.log("presionaste piedra");
+    choiceBtn.innerHTML = "PIEDRA";
+    console.log(getRandomChoice());
+    return 'rock'
 })
+
+paperValue.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("presionaste papel");
+    choiceBtn.innerHTML = "PAPEL"
+    return 'paper'
+})
+
+sicsorsValue.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("presionaste tijeras");
+    choiceBtn.innerHTML = "TIJERAS"
+    return 'sicsors'
+})
+
+
