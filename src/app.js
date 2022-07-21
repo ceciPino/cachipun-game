fb3f5c8de7ded330b40e53ad0575e630c3184f63
ceciPino <mepino@uc.cl>
2022-07-21 10:51:38 -0400

    valor rock

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 //aquí va la función para crear el juego
 //probando
 //probando x2
+
+const rockValue = document.getElementById("rock");
+rockValue.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("presionaste piedra");
+})
