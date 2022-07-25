741ba4e5a8650a91779c6ec905402fc54d498d64
ceciPino <mepino@uc.cl>
2022-07-25 12:03:42 -0400

    opcion computador lista

--- src/app.js
+++ src/app.js
@@ -4,17 +4,28 @@ const paperValue = document.getElementById("paper");
 const sicsorsValue = document.getElementById("sicsors");
 // valor opción seleccionada
 let choiceBtn = document.getElementById("choice");
+let computerResult = document.querySelector(".pc-player")
 
 // Función elección PC
-function getRandomChoice(){
-    // Obtener un número random entre 1 y 3
+function getRandomChoice() {
+    //asignar como valor en una variable un número random entre 1 y 3
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    return randomNumber
+
+    /*usamos switch/case para asignar a cada caso de randomNumber piedra, papel o tijeras
+    por ej: si randomNumber da 2 cuando se ejecuta la función, retornara el valor "paper"*/
+
+    switch (randomNumber) {
+        case 1: computerResult.innerHTML = "PIEDRA"
+            return "rock";
+        case 2: computerResult.innerHTML = "PAPEL"
+            return "paper";
+        case 3: computerResult.innerHTML = "TIJERAS"
+            return "sicsors";
+    }
 }
 
-
 // escuchar la opción marcada por el usuario
-rockValue.addEventListener("click", function(e) {
+rockValue.addEventListener("click", function (e) {
     e.preventDefault();
     console.log("presionaste piedra");
     choiceBtn.innerHTML = "PIEDRA";
@@ -22,17 +33,19 @@ rockValue.addEventListener("click", function(e) {
     return 'rock'
 })
 
-paperValue.addEventListener("click", function(e) {
+paperValue.addEventListener("click", function (e) {
     e.preventDefault();
     console.log("presionaste papel");
-    choiceBtn.innerHTML = "PAPEL"
+    choiceBtn.innerHTML = "PAPEL";
+    console.log(getRandomChoice());
     return 'paper'
 })
 
-sicsorsValue.addEventListener("click", function(e) {
+sicsorsValue.addEventListener("click", function (e) {
     e.preventDefault();
     console.log("presionaste tijeras");
-    choiceBtn.innerHTML = "TIJERAS"
+    choiceBtn.innerHTML = "TIJERAS";
+    console.log(getRandomChoice());
     return 'sicsors'
 })
 
